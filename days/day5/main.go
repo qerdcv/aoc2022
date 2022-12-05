ec4392a6a20e2aebc6b9e537d6bb554874b67877
qerdcv <vadym.tishchenko2@gmail.com>
2022-12-05 23:41:39 +0200

    Implement solution for day 5 part 2

--- days/day5/main.go
+++ days/day5/main.go
@@ -102,21 +102,82 @@ func concatTops(stacks [][]byte) []byte {
 	return result
 }
 
+func calcP1(source io.Reader) ([]byte, error) {
+	stacks, r, err := parseStacks(source)
+	if err != nil {
+		return nil, fmt.Errorf("parse stack: %w", err)
+	}
+
+	stacks, err = executeCommands(stacks, r)
+	if err != nil {
+		return nil, fmt.Errorf("parse stack: %w", err)
+	}
+
+	return concatTops(stacks), nil
+}
+
+func executeCommandsWithSavedDir(stacks [][]byte, r *bufio.Reader) ([][]byte, error) {
+	for {
+		b, _, err := r.ReadLine()
+		if err != nil {
+			if errors.Is(err, io.EOF) {
+				break
+			}
+
+			return nil, fmt.Errorf("reader read line")
+		}
+
+		sCommand := strings.Split(string(b), " ")
+		rawCnt, rawFrom, rawTo := sCommand[1], sCommand[3], sCommand[5]
+		cnt, _ := strconv.Atoi(rawCnt)
+		from, _ := strconv.Atoi(rawFrom)
+		to, _ := strconv.Atoi(rawTo)
+
+		// translate cnt to indexes
+		from--
+		to--
+
+		stacksFromLen := len(stacks[from])
+		stacks[to] = append(stacks[to], stacks[from][stacksFromLen-cnt:]...)
+		stacks[from] = stacks[from][:stacksFromLen-cnt]
+	}
+
+	return stacks, nil
+}
+
+func calcP2(source io.Reader) ([]byte, error) {
+	stacks, r, err := parseStacks(source)
+	if err != nil {
+		return nil, fmt.Errorf("parse stack: %w", err)
+	}
+
+	stacks, err = executeCommandsWithSavedDir(stacks, r)
+	if err != nil {
+		return nil, fmt.Errorf("parse stack: %w", err)
+	}
+
+	return concatTops(stacks), nil
+}
+
 func main() {
 	f, err := os.Open("days/day5/input.txt")
 	if err != nil {
 		panic(err)
 	}
 
-	stacks, r, err := parseStacks(f)
+	result, err := calcP1(f)
 	if err != nil {
 		panic(err)
 	}
 
-	stacks, err = executeCommands(stacks, r)
+	fmt.Println("Part 1: ", string(result))
+
+	f.Seek(0, 0)
+
+	result, err = calcP2(f)
 	if err != nil {
 		panic(err)
 	}
 
-	fmt.Println(string(concatTops(stacks)))
+	fmt.Println("Part 2: ", string(result))
 }
