d98a7524f54a8bfeee9a7048a8214cf71ec2bbeb
qerdcv <vadym.tishchenko2@gmail.com>
2022-12-05 23:33:43 +0200

    Implement solution for day 4 part 2

--- days/day4/main.go
+++ days/day4/main.go
@@ -32,8 +32,6 @@ func checkUnion(u1, u2 Union) bool {
 }
 
 func calcUnions(source io.ReadCloser) (int, error) {
-	defer source.Close()
-
 	unionCnt := 0
 	r := bufio.NewReader(source)
 	for {
@@ -55,6 +53,34 @@ func calcUnions(source io.ReadCloser) (int, error) {
 	return unionCnt, nil
 }
 
+func checkUnionOverlap(u1, u2 Union) bool {
+	return (u1.leftLim >= u2.leftLim && u1.leftLim <= u2.rightLim) || (u1.rightLim >= u2.leftLim && u1.leftLim <= u2.rightLim)
+}
+
+func calcUnionsP2(source io.ReadCloser) (int, error) {
+	defer source.Close()
+
+	unionCnt := 0
+	r := bufio.NewReader(source)
+	for {
+		b, _, err := r.ReadLine()
+		if err != nil {
+			if errors.Is(err, io.EOF) {
+				break
+			}
+			return 0, fmt.Errorf("read line: %w", err)
+		}
+
+		unions := strings.Split(string(b), ",")
+		u1, u2 := NewUnionFromRange(unions[0]), NewUnionFromRange(unions[1])
+		if checkUnionOverlap(u1, u2) {
+			unionCnt++
+		}
+	}
+
+	return unionCnt, nil
+}
+
 func main() {
 	f, err := os.Open("days/day4/input.txt")
 	if err != nil {
@@ -62,4 +88,6 @@ func main() {
 	}
 
 	fmt.Println(calcUnions(f))
+	f.Seek(0, 0)
+	fmt.Println(calcUnionsP2(f))
 }
