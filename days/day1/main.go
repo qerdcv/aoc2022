b9237b41daacfca08cc3604f820ea5bce9eb4778
qerdcv <vadym.tishchenko2@gmail.com>
2022-12-05 22:43:46 +0200

    Implement solution for day 1 part 2

--- days/day1/main.go
+++ days/day1/main.go
@@ -12,21 +12,35 @@ import (
 
 const inputPath = "days/day1/input.txt"
 
-func caloriesCalc(source io.Reader) (int, error) {
+func shiftTop(top [3]int, newTop int) [3]int {
+	if newTop > top[2] {
+		top[0], top[1], top[2] = top[1], top[2], newTop
+		top[2] = newTop
+	} else if newTop > top[1] {
+		top[0], top[1] = top[1], newTop
+	} else if newTop > top[0] {
+		top[0] = newTop
+	}
+	return top
+}
+
+func caloriesCalc(source io.Reader) (int, [3]int, error) {
+	top := [3]int{0, 0, 0}
+
 	result := 0
 	r := bufio.NewReader(source)
 
-main_loop:
-	for {
+	iterating := true
+	for iterating {
 		elfCal := 0
 		for {
 			b, _, err := r.ReadLine()
 			if err != nil {
 				if errors.Is(err, io.EOF) {
-					break main_loop
+					iterating = false
+				} else {
+					return 0, top, fmt.Errorf("reader read line: %w", err)
 				}
-
-				return 0, fmt.Errorf("reader read line: %w", err)
 			}
 
 			line := string(b)
@@ -36,17 +50,18 @@ main_loop:
 
 			cal, err := strconv.Atoi(line)
 			if err != nil {
-				return 0, fmt.Errorf("strconv a to i: %w", err)
+				return 0, top, fmt.Errorf("strconv a to i: %w", err)
 			}
 			elfCal += cal
 		}
 
+		top = shiftTop(top, elfCal)
 		if elfCal > result {
 			result = elfCal
 		}
 	}
 
-	return result, nil
+	return result, top, nil
 }
 
 func main() {
@@ -55,10 +70,10 @@ func main() {
 		log.Fatalln(fmt.Errorf("os open: %w", err).Error())
 	}
 
-	result, err := caloriesCalc(f)
+	result, top, err := caloriesCalc(f)
 	if err != nil {
 		log.Fatalln(err.Error())
 	}
 
-	log.Printf("The result is: %d\n", result)
+	log.Printf("The result is: %d %d\n", result, top[0]+top[1]+top[2])
 }
